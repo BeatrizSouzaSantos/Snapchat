8f0e459b25388a0bf64d167bc7bf6811bef0db15
Beatriz Souza <beatrizsouzasantos1288@gmail.com>
2021-10-27 15:17:30 -0300

    Initial Commit

---
+++ Snapchat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Snapchat
+//
+//  Created by user on 27/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
